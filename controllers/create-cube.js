ad154d4ed677ccaa1cc58f70e386175cb0468e8a
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-10-09 16:55:31 +0300

    small fixes

--- controllers/create-cube.js
+++ controllers/create-cube.js
@@ -2,6 +2,6 @@ const Cube = require('../models/cube');
 
 const newCube = new Cube('default', 'this is def cube', 'http://google.com', '1')
 
-console.log(newCube);
+//console.log(newCube);
 //запазваме файла, като използваме функцията от cube.js
 newCube.save()
