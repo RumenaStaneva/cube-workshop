850ceb619c58158dc523ea935a4e9588c19ee85a
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-10-09 16:21:44 +0300

    feat: create-cub-model and save cube in db

---
+++ controllers/create-cube.js
@@ -0,0 +1,7 @@
+const Cube = require('../models/cube');
+
+const newCube = new Cube('default', 'this is def cube', 'http://google.com', '1')
+
+console.log(newCube);
+//запазваме файла, като използваме функцията от cube.js
+newCube.save()
